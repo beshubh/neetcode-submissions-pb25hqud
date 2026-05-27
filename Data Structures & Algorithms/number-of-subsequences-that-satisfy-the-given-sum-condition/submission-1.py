d4a10e94dd2a4966059f061fa5165dd723c8e489
shubh <39399239+beshubh@users.noreply.github.com>
2026-05-28 01:07:04 +0530

    Add: number-of-subsequences-that-satisfy-the-given-sum-condition - submission-1

---
+++ Data Structures & Algorithms/number-of-subsequences-that-satisfy-the-given-sum-condition/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        result = 0
+        mod = (10**9 + 7)
+        for i in range(len(nums)):
+            # get all the subsequences with min and max value <= target
+            left = nums[i]
+            r = len(nums) - 1
+            # if left + right are <= target that means all the subsequences in that 
+            # window are valid which is 2^(size of window)
+            while left + nums[r] > target and r >= i: # r = i is valid, because subseq can be of length one as well
+                r -= 1
+            
+            if r >= i:
+                result += (1 << (r - i))
+                result %= mod
+
+        return result 
+            
