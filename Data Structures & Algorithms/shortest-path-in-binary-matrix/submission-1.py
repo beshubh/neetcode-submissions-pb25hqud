7c1c2c56869a2f558d1d867f2496a92d918712b0
shubh <39399239+beshubh@users.noreply.github.com>
2026-06-03 09:38:57 +0530

    Add: shortest-path-in-binary-matrix - submission-1

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        # bfs, process layer by layer, diagonals are also possible
+        NEIGHBORS = [
+            (0, 1),
+            (0, -1),
+            (1, 1),
+            (-1, -1),
+            (1, -1),
+            (-1, 1),
+            (1, 0),
+            (-1, 0)
+        ]
+        N = len(grid)
+        if grid[0][0] == 1 or grid[N - 1][N - 1] == 1:
+            return -1
+
+        q = deque([(0, 0)])
+        seen = set()
+        length = 1
+        while q:
+            qlen = len(q)
+            for _ in range(qlen):
+                r, c = q.popleft()
+                if (r, c) in seen:
+                    continue
+                seen.add((r, c))
+                for dr, dc in NEIGHBORS:
+                    row, col = r + dr, c + dc
+                    if (
+                        row >= 0 and row < N and col >= 0 and col < N and
+                        (row, col) not in seen
+                    ):  
+                        if row == N - 1 and col == N - 1:
+                            return length + 1
+                        if grid[row][col] == 0:
+                            q.append((row, col))
+            length += 1
+        return -1
