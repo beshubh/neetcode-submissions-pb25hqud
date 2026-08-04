9af8299892a6271fb01e02c60751011cc1b8e414
shubh <39399239+beshubh@users.noreply.github.com>
2026-08-04 10:54:21 +0530

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        cache = {}
+        total_sum = sum(nums)
+        def go(i: int, a: int, b: int):
+            key = (i, a, b)
+            if i >= len(nums):
+                return a == b
+            if key in cache:
+                return cache[key]
+            c1 = go(i + 1, a + nums[i], b - nums[i])
+            c2 = go(i + 1, a, b) 
+            cache[key] = c1 or c2
+            return c1 or c2
+        return go(0, 0, total_sum)
